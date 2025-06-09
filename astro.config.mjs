8e2b95506d3b76fa607fe0050a94ec995b38aed6
Max <mbustamang@eafit.edu.co>
2025-06-09 00:42:34 -0500

    removed react dependencies for not loading js

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
-import react from '@astrojs/react';
 import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), icon()],
+  integrations: [icon()],
   vite: {    plugins: [tailwindcss()],  },
 });
